8473fb36b252bbb98062cfa54d7c49d5243d8349
Alvino Abyan Rizaldi <162532484+sweethoneybuckin@users.noreply.github.com>
2025-03-23 07:08:20 +0700

    Delete Backend directory

--- Backend/config/Database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('RECOVER_YOUR_DATA', 'root','password',{
-    host: '34.128.101.39',
-    dialect : 'mysql'
-});
-
-export default db;
