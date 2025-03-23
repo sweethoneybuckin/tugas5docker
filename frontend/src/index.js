ec7dd14055425b2e5a56594015ee6d3eccb2ffb1
Alvino Abyan Rizaldi <162532484+sweethoneybuckin@users.noreply.github.com>
2025-03-23 07:08:11 +0700

    Delete frontend directory

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bulma/css/bulma.min.css';  // Impor Bulma
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
