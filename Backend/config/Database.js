aacbdc84bf96ecadba2477783376ad5b4dacb7f2
vino <rizaldialvino22@gmail.com>
2025-04-09 18:24:19 +0700

    fix: changed

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('RECOVER_YOUR_DATA', 'root','',{
+const db = new Sequelize('RECOVER_YOUR_DATA', 'root','password',{
     host: '34.128.101.39',
     dialect : 'mysql'
 });
